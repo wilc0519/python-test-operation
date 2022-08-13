4086b56e45913206c0176e77f2a7a6dc67819e37
wilc0519 <wilc0519@gmail.com>
2022-08-13 15:54:56 -0500

    5 commit of first branch

--- suma_numeros.py
+++ suma_numeros.py
@@ -2,3 +2,7 @@ def sum_numbers(numbers = None):
     if numbers is None:
         return sum(range(1, 101))
     return sum(numbers)
+
+def prod_numbers(number, number2):
+    prod = number * number2
+    return prod
