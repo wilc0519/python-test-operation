a0f8d3a457dc687d420058e63fda7752287aa309
wilc0519 <wilc0519@gmail.com>
2022-08-13 21:07:06 -0500

    10 commit of second branch

--- factorial.py
+++ factorial.py
@@ -2,7 +2,7 @@ from math import factorial
 
 
 def factorial_number(number):
-    if number ==  (0 or 1):
+    if (number == 0 or  number == 1)  :
         return 1
-    else:
-        return number * factorial(number-1)      
+    
+    return number * factorial(number-1)      
--- test_factorial.py
+++ test_factorial.py
@@ -2,18 +2,18 @@ import unittest
 from factorial import factorial_number
 
 class TestFactorial(unittest.TestCase):
-    def test_factorial(self):
+    def test_factorial_numbers_greater_than_one(self):
         number = 3
         fact = factorial_number(number)
         self.assertEqual(fact, 6)
 
-    def test_factorial(self):
+    def test_factorial_of_uno(self):
         number = 1
         fact = factorial_number(number)
         self.assertEqual(fact, 1)   
 
-    def test_factorial(self):
+    def test_factorial_of_cero(self):
         number = 0
         fact = factorial_number(number)
-        self.assertEqual(fact, 0)     
+        self.assertEqual(fact, 1)     
 
