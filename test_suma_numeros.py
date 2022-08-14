1146c64a8e20b4cfc3031140f267b4c3b943c467
wilc0519 <wilc0519@gmail.com>
2022-08-13 19:39:04 -0500

    1 commit of second branch

--- test_suma_numeros.py
+++ test_suma_numeros.py
@@ -1,5 +1,5 @@
 import unittest
-from suma_numeros import sum_numbers
+from suma_numeros import sum_numbers, prod_numbers
 
 class TestSuma(unittest.TestCase):
     def test_sum_numbers_default_args(self):
@@ -12,6 +12,13 @@ class TestSuma(unittest.TestCase):
         sum = sum_numbers(numbers)
         self.assertEqual(sum,10)
         self.assertEqual(sum_numbers(range(1,11)), 55)
+
+    def test_prod_two_numbers(self):
+        first_number = 4
+        second_number = 7
+        prod = prod_numbers(first_number, second_number)
+        self.assertEqual(prod,28)
+
         
 
 if __name__ == '__main__':
