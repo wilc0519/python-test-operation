5f4dedd564d5e0948186a5797906682dbc0e3234
wilc0519 <wilc0519@gmail.com>
2022-08-13 20:34:51 -0500

    5 commit of second branch

--- test_factorial.py
+++ test_factorial.py
@@ -1,7 +1,8 @@
+import unittest
+from suma_numeros import sum_numbers, prod_numbers
 
-from factorial import factorial_number
-
-def test_factorial(self):
-    number = 3
-    self.assertEquals
+class TestFactorial(unittest.TestCase):
+    def test_factorial(self):
+        number = 3
+        self.assertEqual(number, 6)
 
