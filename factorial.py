c5ac98eaeb2280d3750cbd4704db7d136ce2bd4b
wilc0519 <wilc0519@gmail.com>
2022-08-13 21:10:58 -0500

    12 commit of second branch

--- factorial.py
+++ factorial.py
@@ -2,7 +2,10 @@ from math import factorial
 
 
 def factorial_number(number):
-    if (number == 0 or  number == 1)  :
+    if number == 0:
+        return 1
+
+    if number == 1:
         return 1
     
     return number * factorial(number-1)      
